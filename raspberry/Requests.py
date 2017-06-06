6fe705a43e223a445d2d70bd21c9c64f0d096c62
Raul Villca <raulvillca@MacBook-Pro-de-Raul.local>
2017-06-06 00:00:11 -0300

    script para peticiones a servidor firebase

---
+++ raspberry/Requests.py
@@ -0,0 +1,26 @@
+import json
+import requests
+from firebase import firebase
+
+def send_notification(token_gcm, title, message):
+    data = {
+        'to': 'd9Y5oO9Ol9A:APA91bHViajMMu2IUavIGuQzg42S4UfsIMcVGkyMsCCVYJCsS3jHot4ijYUPpvArbTzTMpHaeN7jorlQCy7Wa9oFnqQBQiGIuU03oOeLsO42eJS44W0j-HiByrBjQ-uLJ-M-oYepX051',
+        'notification': {'title': 'Aviso importante', 'text': 'Tu tiempo se esta agotando'}
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "key=AAAAWUF2oGQ:APA91bFNkIO0-qlzhNL3oYFAhN6mpaYBPJD0P1zMMNuB2BblI7Md5xjYlLgfJKlcPLZ9LOiXULQLQz0h8M-Zx-8-U2aayr1-jEIEYl1fnT42rahEkiimUNtsdlcEsElFb3hsPu268nMk"
+    }
+    url = 'https://fcm.googleapis.com/fcm/send'
+
+    r = requests.post(url, data=json.dumps(data), headers=headers)
+    print(r.text)
+
+def connectFirebase():
+    firebase = firebase.FirebaseApplication('https://automatedparking-43b58.firebaseio.com/')
+
+def getReservations():
+    arrayParking = {}
+    arrayParking['parking_a'] = firebase.get('/parking_a/unlam/times', None)
+    arrayParking['parking_b'] = firebase.get('/parking_a/unlam/times', None)
+    return arrayParking;
