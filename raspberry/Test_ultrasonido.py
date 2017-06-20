a7171ea8ceb0ed68818f861ad7f453d53018d90d
Raul Villca <raulvillca@MacBook-Pro-de-Raul.local>
2017-06-20 17:18:29 -0300

    test

--- raspberry/Test_ultrasonido.py
+++ raspberry/Test_ultrasonido.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-""""
-Tomado en gran parte de:
-https://electrosome.com/hc-sr04-ultrasonic-sensor-raspberry-pi/
-Formula para calcular la distancia
-d = V*(t/2)
-V = velocidad del sonido
-t = tiempo, que tarda la señal de ir del emisor al obstaculo y volver al receptor
-""
 
 ##oximetria
 import RPi.GPIO as GPIO
@@ -25,17 +17,17 @@ GPIO.setup(ECHO,GPIO.IN)                   #ECHO como entrada
 
 GPIO.output(TRIG, False)                   #TRIG en estado bajo
 print "Espere que el sensor se estabilice"
-time.sleep(2)                              #Esperar 2 segundos
+time.sleep(1)                              #Esperar 2 segundos
 
 GPIO.output(TRIG, True)                    #TRIG en estado alto
 time.sleep(0.00001)                        #Delay de 0.00001 segundos
 GPIO.output(TRIG, False)                   #TRIG en estado bajo
 
-while GPIO.input(ECHO)==0:                 #Comprueba si ECHO está en estado bajo
-  pulse_start = time.time()                #Guarda el tiempo transcurrido, mientras esta en estado bajo
+while GPIO.input(ECHO)==0:                 #Comprueba si ECHO esta en estado bajo
+    pulse_start = time.time()                #Guarda el tiempo transcurrido, mientras esta en estado bajo
 
-while GPIO.input(ECHO)==1:                 #Comprueba si ECHO está en estado alto
-  pulse_end = time.time()                  #Guarda el tiempo transcurrido, mientras esta en estado alto
+while GPIO.input(ECHO)==1:                 #Comprueba si ECHO esta en estado alto
+    pulse_end = time.time()                  #Guarda el tiempo transcurrido, mientras esta en estado alto
 
 t = pulse_end - pulse_start                #Se obtienen la duración del pulso, calculando la diferencia entre pulse_start  y pulse_end
 
@@ -44,10 +36,11 @@ distancia = round(distancia, 2)            #Se redondea a dos decimales
 
 if distancia > 2 and distancia < 400:      #Comprueba si la distancia está dentro del rango
 
-  print "Distancia: ",distancia,"cm"       #Imprime la distancia
+
+    print "Distancia: ",distancia,"cm"       #Imprime la distancia
 
 else:
   print "Fuera de Rango"                   #Imprime fuera de rango
 
 GPIO.cleanup()							   #Limpia los pines
-"""""
+
