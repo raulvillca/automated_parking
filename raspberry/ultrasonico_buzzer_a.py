76e85cc4a873ff7d8fbb2abb4263b8d8651ddde9
Raul Villca <raulvillca@MacBook-Pro-de-Raul.local>
2017-06-20 22:01:15 -0300

    sin comentarios

--- raspberry/ultrasonico_buzzer_a.py
+++ raspberry/ultrasonico_buzzer_a.py
@@ -41,10 +41,10 @@ def ultrasonico_buzzer_a(mode):
 
     if distancia > 2 and distancia < 400:
 
-        if 2 < distancia < 3:
+        if 2 < distancia < 3.6:
             GPIO.output(BUZZER_A, True)
             time.sleep(0.5)
-        elif distancia > 3.5:
+        elif distancia > 3.6:
             GPIO.output(BUZZER_A, False)
 
         print "Distancia: ",distancia,"cm"
