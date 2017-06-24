10ff1051bb9e76b28290366ac66a2e2ecf4dbf20
Raul Villca <raulvillca@MacBook-Pro-de-Raul-2.local>
2017-06-24 19:08:05 -0300

    sin comentarios

--- raspberry/Requests.py
+++ raspberry/Requests.py
@@ -24,20 +24,20 @@ def send_notification(time_firebase, token_gcm, title, message):
     hh_fb, mm_fb = time_firebase.split(':')
     hh_now, mm_now = time_now.split(':')
 
-    hh_fb_int = int(hh_fb)*100
+    hh_fb_int = int(hh_fb)*3600
     hh_fb_int = hh_fb_int + int(mm_fb)
 
-    hh_now_int = int(hh_now)*100
+    hh_now_int = int(hh_now)*3600
     hh_now_int = hh_now_int + int(mm_now) - 10
 
 
     print str(hh_fb_int), str(hh_now_int)
 
-    if hh_fb_int <= hh_now_int:
-        return False;
-    else:
+    if hh_fb_int > hh_now_int:
         r = requests.post(url, data=json.dumps(data), headers=headers)
         print(r.text)
+        return False;
+    else:
         return False;
 
 def getNotifications():
