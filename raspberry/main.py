8d09ae4097f43fca9587f59c1916a2f35d3e05a3
Raul Villca <raulvillca@MacBook-Pro-de-Raul-2.local>
2017-06-28 22:03:21 -0300

    hilo luces

--- raspberry/main.py
+++ raspberry/main.py
@@ -11,30 +11,12 @@ import ultrasonico_buzzer_b
 import RPi_I2C_driver
 import infra_servo
 
+
+
 piezo = 29
 fotocelda = 31
-GPIO.setmode(GPIO.BOARD)
-
-GPIO.setup(piezo,GPIO.OUT)
-GPIO.setup(fotocelda,GPIO.IN)
 
 current_state = 0
-
-def luces(valor):
-	try:
-		while True:
-			print GPIO.input(fotocelda)
-			if GPIO.input(fotocelda) == 1 :
-				time.sleep(0.1)
-				GPIO.output(piezo,True)
-				print("encendido")
-				time.sleep(2)
-			else:
-				GPIO.output(piezo,False)
-				print("apagado")
-
-	except KeyboardInterrupt:
-		pass
 
 IS_ACTIVE = True
 HAY_MSJ = False
